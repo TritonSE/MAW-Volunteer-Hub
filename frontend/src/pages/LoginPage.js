7663ffdb381aa75566e1f148ddac62953d7a8483
Cubified <andrewrussell2002@gmail.com>
2022-01-10 14:29:03 -0800

    Fix responsiveness issues using CSS media queries

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../index.css";
 import "../css/LoginPage.css";
 
 function PasswordField({ placeholder, className, onChange }) {
@@ -9,7 +10,7 @@ function PasswordField({ placeholder, className, onChange }) {
       <input placeholder={placeholder} type={isVisible ? "text" : "password"} onChange={onChange} />
       <img
         alt="Toggle password visibility"
-        src={isVisible ? "/visible.svg" : "/notvisible.svg"}
+        src={isVisible ? "/img/login_visible.svg" : "/img/login_notvisible.svg"}
         className={"login_password_eye" + (isVisible ? " visible" : "")}
         onClick={() => setIsVisible(!isVisible)}
       />
@@ -30,37 +31,38 @@ function LoginPage() {
 
   return (
     <div className="login">
-      <img alt="Make-a-Wish Logo" src="/login_logo.svg" className="login_logo" />
+      <img alt="Make-a-Wish Logo" src="/img/login_logo.svg" className="login_logo" />
       <form className="login_box" action="/">
-        <input
-          placeholder="Full Name"
-          className={isLogin ? "hidden" : ""}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input placeholder="Email" type="email" onChange={(e) => setEmail(e.target.value)} />
-        <PasswordField
-          placeholder="Password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <div>
+          <input
+            placeholder="Full Name"
+            className={isLogin ? "hidden" : ""}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input placeholder="Email" type="email" onChange={(e) => setEmail(e.target.value)} />
+          <PasswordField
+            placeholder="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <PasswordField
-          placeholder="Re-enter password"
-          type="password"
-          className={isLogin ? "hidden" : ""}
-          onChange={(e) => setRPassword(e.target.value)}
-        />
-        <div className={"login_flex" + (isLogin ? "" : " hidden")}>
-          <div className="login_flex_center">
-            <input type="checkbox" id="remember" />
-            <label htmlFor="remember">Keep me signed in</label>
+          <PasswordField
+            placeholder="Re-enter password"
+            type="password"
+            className={isLogin ? "hidden" : ""}
+            onChange={(e) => setRPassword(e.target.value)}
+          />
+          <div className={"login_flex" + (isLogin ? "" : " hidden")}>
+            <div className="login_flex_center">
+              <input type="checkbox" id="remember" />
+              <label htmlFor="remember">Keep me signed in</label>
+            </div>
+            <a href="#forgot">Forgot password</a>
           </div>
-          <a href="#forgot">Forgot password</a>
+          <button type="submit" disabled={!isLogin && !validate()}>
+            {isLogin ? "Login" : "Create new account"}
+          </button>
         </div>
-        <button type="submit" disabled={!isLogin && !validate()}>
-          {isLogin ? "Login" : "Create new account"}
-        </button>
-
         <button type="button" className="login_switch" onClick={() => setIsLogin(!isLogin)}>
           {isLogin ? "Create new account" : "I already have an account"}
         </button>
