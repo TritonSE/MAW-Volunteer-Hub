28c233e4089dd4ce97ec23ec276f87c0c7625f51
mohakvni <68292499+mohakvni@users.noreply.github.com>
2022-01-14 07:01:30 +0300

    Create User_verify.js
    Used express to change verified to true for a particular user using their unique userID.

---
+++ backend/routes/User_verify.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const route = express.Router()
+const {User} = require("./model")
+
+route.get("/:id", (req,res)=>{
+    try{
+        User.findById(req.params.id).then((user) => {
+            if (user.verified){
+                res.send("User already approved")
+            }else{
+                Object.assign(user, {verified : true})
+                user.save()
+                res.send("User succesfully approved")
+            }  
+        })
+    }catch{
+        res.status(404).send({error: "user not found"})
+    }
+})
+
+
+module.exports = route
