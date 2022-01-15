51f474d0505e0e159169d2c6cec8bf9ed0bc9bc3
aksharans <aksharan@gmail.com>
2022-01-14 16:53:12 -0800

    html/styling adjustments

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,7 +3,7 @@
  */
 import React, { useState } from "react";
 import { SITE_PAGES } from "../constants/links";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles/NavBar.css";
 
 const PAGES = {
@@ -16,9 +16,10 @@ const PAGES = {
 
 function NavBar() {
 
+    const [dropdown, setDropdown] = useState(false);
+
     const signout = () => {};
 
-    const [dropdown, setDropdown] = useState(false);
 
     return (
 
@@ -28,54 +29,43 @@ function NavBar() {
 
             <li className="logo-container">
                 <NavLink to={SITE_PAGES.MANAGE}>
-                    <img src="/img/maw_logo.svg" alt="Make-A-Wish logo" className="maw-logo"/>
+                    <img src="/img/maw_logo.png" alt="Make-A-Wish logo" className="maw-logo"/>
                 </NavLink>
             </li>
 
-
-
             <li className="pages-container">
-            { Object.entries(PAGES).map( ([page, route]) => (
-                
-                <li className="page-name">
+                { Object.entries(PAGES).map( ([page, route]) => (
                     <NavLink className="page-links" activeClassName="underline" to={route}>{page}</NavLink>
-                </li>
-
-            ))}
+                ))}
             </li>
-            
 
-        <li className="search-and-profile">
-            
-            <div className="search-container">
-                <input className="search-input" placeholder="Search all files..."/>
-                <button className="search-button" onClick={() => window.location.reload()}>
-                    <img src="/img/searchbar.svg" alt="Search Bar" className="searchbar"/>
-                </button>
-            </div>
+            <li className="search-and-profile">
+                
+                <form className="search-container">
+                    <input className="search-input" placeholder="Search all files..."/>
+                    <button className="search-button" type="submit">
+                        <img src="/img/searchbar.svg" alt="Search" className="searchbar-icon"/>
+                    </button>
+                </form>
 
-            <div className="profile-container">
+                <div className="profile-container">
 
-                <div className="profile-icon" onClick={() => setDropdown(prev => !prev)}>
-                    <img src="/img/profile_icon.svg" alt="Profile Icon" className="account-icon"/>
-                    <img src="/img/dropdown_icon.svg" alt="Arrow Dropdown" className="arrow-dropdown"/>
-                </div>
+                    <div className="profile-icon" onClick={() => setDropdown(prev => !prev)}>
+                        <img src="/img/profile_icon.svg" alt="Profile Icon" className="account-icon"/>
+                        <img src="/img/dropdown_icon.svg" alt="Arrow Dropdown" className="arrow-dropdown"/>
+                    </div>
 
-                {dropdown && (
-                    <div className="profile-dropdown">
-                        <div className="view-profile">
+                    {dropdown && (
+                        <div className="profile-dropdown">
                             <NavLink className="view-profile-link" to={SITE_PAGES.PROFILE}>View your profile</NavLink>
-                        </div>
-                        <div onClick={() => signout()}>
-                            <NavLink className="signout-link" to={SITE_PAGES.LOGIN}>
+                            <NavLink className="signout-link" to={SITE_PAGES.LOGIN} onClick={() => signout()}>
                                 <span>Sign Out</span>
                                 <img src="/img/signout_icon.svg" alt="Sign out icon" className="signout-icon"/>
                             </NavLink>
                         </div>
-                    </div>
-                )}
+                    )}
 
-            </div>
+                </div>
             </li>
 
         </ul>
