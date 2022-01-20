01e550e535df8dd66570f3c171eb785e0f219f5b
James Zhang <jjz002@ucsd.edu>
2022-01-19 20:21:36 -0800

    Import react table package basics

--- frontend/src/pages/WishGrantingPage.js
+++ frontend/src/pages/WishGrantingPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import UserList from "../components/UserList";
 
 function WishGrantingPage() {
   return (
     <div>
       <h1>Wish Granting</h1>
+      <UserList />
     </div>
   );
 }
