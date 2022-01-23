82abd7a3651608db11c4863253c64ee33bcb37e4
James Zhang <jjz002@ucsd.edu>
2022-01-22 19:51:10 -0800

    Move UserList to PeoplePage

--- frontend/src/pages/PeoplePage.js
+++ frontend/src/pages/PeoplePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import UserList from "../components/UserList";
 
 function PeoplePage() {
   return (
     <div>
       <h1>People</h1>
+      <UserList />
     </div>
   );
 }
