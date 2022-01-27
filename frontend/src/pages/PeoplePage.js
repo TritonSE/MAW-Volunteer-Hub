fd00b745f7f0c85ff4dca9386baf1328be6f7057
James Zhang <jjz002@ucsd.edu>
2022-01-26 18:58:57 -0800

    Add side nav to people page

--- frontend/src/pages/PeoplePage.js
+++ frontend/src/pages/PeoplePage.js
@@ -1,11 +1,23 @@
 import React from "react";
 import UserList from "../components/UserList";
+import SideNav from "../components/SideNav";
 
 function PeoplePage() {
+  const tabs = [
+    {
+      tab_name: "People",
+      tab_content: <UserList />,
+    },
+    {
+      tab_name: "Places",
+      tab_content: <UserList />,
+    },
+  ];
   return (
     <div>
-      <h1>People</h1>
-      <UserList />
+      <div style={{ background: "#f7f7f7" }}>
+        <SideNav tabs={tabs} />
+      </div>
     </div>
   );
 }
