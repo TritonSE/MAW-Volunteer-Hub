871e34eb074571ca3a6963ca0d2e4edd1ecb4afa
aksharans <aksharan@gmail.com>
2022-01-29 21:20:56 -0800

    added temp mobile styles

--- frontend/src/components/NavMenuMobile.js
+++ frontend/src/components/NavMenuMobile.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { PAGES } from "../constants/pages";
+import { SITE_PAGES } from "../constants/links";
 import "../styles/NavMenuMobile.css";
 
 function NavMenuMobile({ desktopDropdown, setDesktopDropdown }) {
   return (
     <div className="nav-menu-mobile">
-      <div className="mobile-search">
+      <div className="search-mobile">
         <button type="button" className="search-button-mobile">
           <img src="/img/searchbar.svg" alt="Search" className="searchbar-icon-mobile" />
         </button>
       </div>
 
-      <div className="mobile-dropdown">
+      <div className="dropdown-icon-mobile">
         <button
           type="button"
           className="dropdown-button-mobile"
@@ -24,9 +25,9 @@ function NavMenuMobile({ desktopDropdown, setDesktopDropdown }) {
 
       {/* Mobile dropdown, uses same state as desktop dropdown */}
       {desktopDropdown && (
-        <div className="dropdown-container">
-          <div className="logo-and-exit">
-            <img src="/img/maw_logo.png" alt="Make-A-Wish logo" className="maw-logo" />
+        <div className="dropdown-container-mobile">
+          <div className="logo-and-exit-mobile">
+            <img src="/img/maw_logo.png" alt="Make-A-Wish logo" className="maw-logo-mobile" />
             <button
               type="button"
               className="exit-dropdown-mobile"
@@ -35,25 +36,44 @@ function NavMenuMobile({ desktopDropdown, setDesktopDropdown }) {
               <img
                 src="../img/close_menu_btn.svg"
                 alt="Exit Dropdown Menu"
-                className="close-menu-btn"
+                className="close-menu-btn-mobile"
               />
             </button>
           </div>
 
-          <hr className="separator" />
+          <hr className="separator-mobile" />
 
           <div className="pages-container-mobile">
             {Object.entries(PAGES).map(([page, route]) => (
               <NavLink
                 key={route}
                 className="page-links-mobile"
-                activeClassName="underline"
+                activeClassName="underline-mobile"
                 to={route}
+                onClick={() => setDesktopDropdown((prevState) => !prevState)}
               >
                 {page}
               </NavLink>
             ))}
           </div>
+
+          <hr className="separator-mobile" />
+
+          <NavLink
+            className="view-profile-link-mobile"
+            to={SITE_PAGES.PROFILE}
+            onClick={() => setDesktopDropdown((prevState) => !prevState)}
+          >
+            View your profile
+          </NavLink>
+          <NavLink
+            className="signout-link-mobile"
+            to={SITE_PAGES.LOGIN}
+            onClick={() => setDesktopDropdown((prevState) => !prevState)}
+          >
+            <span>Sign Out</span>
+            <img src="/img/signout_icon.svg" alt="Sign out icon" className="signout-icon-mobile" />
+          </NavLink>
         </div>
       )}
     </div>
