94c4638dee8fcb4dd02aaad886ac64424b1565e4
PatrickBrown1 <patrickbrown1312@gmail.com>
2022-01-29 16:50:45 -0800

    fixed linting errors

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,22 +4,19 @@ const router = express.Router();
 const { UserModel } = require("../models/UserModel");
 
 // temporary secure route, accessed with /users/
-router.get("/secure", (req, res, next) => {
+router.get("/secure", (req, res, next) =>
   res.json({
     message: "You made it to the secure route",
     user: req.user,
     token: req.query.secret_token,
-  });
-});
+  })
+);
 
 router.get("/users/:admin", (req, res, next) => {
-  if (req.params.admin === true || req.params.admin === false){
-    UserModel.find({ admin: req.params.admin }).then((user) => {
-    res.json(user);
-    });
+  if (req.params.admin === true || req.params.admin === false) {
+    const users = UserModel.find({ admin: req.params.admin });
+    return res.json(users);
   }
-  else{
-    return res.status(400).json({error: 'Malformed Input' });
-  }
+  return res.status(400).json({ error: "Malformed Input" });
 });
 module.exports = router;
