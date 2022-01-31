20bb0c09c6321f6dc818953448fee1e9dc6977b2
shreyagupta112 <94256244+shreyagupta112@users.noreply.github.com>
2022-01-30 20:09:37 -0800

    Feature/shreyagupta112/created user model integrted passport js and made admin get route (#34)
    * User Schema and Admin get route
    
    User Schema and get route for admin and non-admin users
    
    * Created admin.js
    
    Get route which retrieves a list of all admin users or a list of all non-admin users.
    
    * Created User model
    
    * Debugging
    
    Removing unused exports and fixing incorrect path name
    
    * added temp work
    
    * fixed linting errors
    
    * updated file structure
    
    * Signup, authentication, and secure routes functional
    
    * Made requested changes to UserModel.js and UserRoutes
    
    * UserRoutes Update
    
    Modified the get route for admin/ non-admin users to check if req.params.admin is a valid input
    
    * Update to UserRoutes
    
    * Fix UserRoutes
    
    * fixed linting errors
    
    Co-authored-by: PatrickBrown1 <patrickbrown1312@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+
+// const UserModel = require("./models/model");
+
+mongoose.connect("mongodb://127.0.0.1:27017/MAWDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+mongoose.connection.on("error", (error) => console.log(error));
+mongoose.Promise = global.Promise;
+
+require("./auth/passportutil");
+
+const authRoutes = require("./routes/AuthRoutes");
+const userRoutes = require("./routes/UserRoutes");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use("/auth", authRoutes); // authentication routes are not JWT protected
+app.use("/user", passport.authenticate("jwt", { session: false }), userRoutes); // all user routes are JWT protected
+
+// Handle errors.
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err });
 });
 
-app.use((req, res, next) => {
-  res.send("Welcome to Express");
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(5000, () => {
+  console.log("Server started.");
 });
