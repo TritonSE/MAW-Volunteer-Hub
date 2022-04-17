3078b7164a1e64d343572db38e1094559649b3ec
James Zhang <jjz002@ucsd.edu>
2022-04-17 15:37:01 -0700

    Add mobile adaptability to first two components

--- frontend/src/components/ProfleCompleted.js
+++ frontend/src/components/ProfleCompleted.js
@@ -4,7 +4,7 @@ import "../styles/ProfileCompleted.css";
 export default function ProfileCompleted(props) {
   return (
     <div className="assign_completed">
-      <h2>Assignments Completed </h2>
+      <h2 className="task_title">Assignments Completed </h2>
       <p className="task_number">{props.tasks}</p>
     </div>
   );
