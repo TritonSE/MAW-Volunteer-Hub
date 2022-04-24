d70e3e86352021314602b07f7b907b46a89e2f89
James Zhang <jjz002@ucsd.edu>
2022-04-24 16:12:10 -0700

    Add style to activity log

--- frontend/src/components/ProfileActivities.js
+++ frontend/src/components/ProfileActivities.js
@@ -4,16 +4,35 @@ import { useTable } from "react-table";
 
 import "../styles/ProfileActivities.css";
 
+// Dummy Data. When the backend routes are done, the data should be passed in through props.
 const data = [
-  { Date: "02/15/21", Event: "Backyard Makeover", Hours: 2 },
-  { Date: "02/15/21", Event: "Backyard Makeover1", Hours: 3 },
-  { Date: "02/15/21", Event: "Backyard Makeover2", Hours: 4 },
-  { Date: "02/15/21", Event: "Backyard Makeover3", Hours: 5 },
-  { Date: "02/15/21", Event: "Backyard Makeover4", Hours: 6 },
-  { Date: "02/15/21", Event: "Backyard Makeover5", Hours: 7 },
-  { Date: "02/15/21", Event: "Backyard Makeover6", Hours: 8 },
-  { Date: "02/15/21", Event: "Backyard Makeover7", Hours: 9 },
-  { Date: "02/15/21", Event: "Backyard Makeover8", Hours: 11 },
+  { Date: "02/15/21", Event: "Backyard Makeover", Hours: 2, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover1", Hours: 3, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover2", Hours: 4, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover3", Hours: 5, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover4", Hours: 6, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover5", Hours: 7, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover6", Hours: 8, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover7", Hours: 9, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover8", Hours: 11, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover", Hours: 2, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover1", Hours: 3, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover2", Hours: 4, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover3", Hours: 5, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover4", Hours: 6, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover5", Hours: 7, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover6", Hours: 8, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover7", Hours: 9, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover8", Hours: 11, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover", Hours: 2, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover1", Hours: 3, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover2", Hours: 4, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover3", Hours: 5, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover4", Hours: 6, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover5", Hours: 7, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover6", Hours: 8, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover7", Hours: 9, Id: "BackyardMakeOver123" },
+  { Date: "02/15/21", Event: "Backyard Makeover8", Hours: 11, Id: "BackyardMakeOver123" },
 ];
 
 // Pass in through props?
@@ -30,14 +49,26 @@ const columns = [
     Header: "Hours",
     accessor: "Hours",
   },
+  // {
+  //   Header: "",
+  //   accessor: "Id",
+  // },
 ];
 
 export default function ProfileActivities(props) {
-  // Dummy Data. When the backend routes are done, the data should be passed in through props.
-
   const tableInstance = useTable({ columns, data });
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+
+  const getHeaderSuffix = (colIndex) => {
+    if (colIndex === 0) {
+      return "start";
+    }
+    if (colIndex === columns.length - 1) {
+      return "end";
+    }
+    return "";
+  };
 
   return (
     <div>
@@ -46,8 +77,13 @@ export default function ProfileActivities(props) {
         <thead>
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              {headerGroup.headers.map((column, colIndex) => (
+                <th
+                  {...column.getHeaderProps()}
+                  className={`activities_table_header ${getHeaderSuffix(colIndex)}`}
+                >
+                  {column.render("Header")}
+                </th>
               ))}
             </tr>
           ))}
@@ -64,9 +100,12 @@ export default function ProfileActivities(props) {
                 <tr {...row.getRowProps()}>
                   {
                     // Loop over the rows cells
-                    row.cells.map((cell) => (
+                    row.cells.map((cell, colIndex) => (
                       // Apply the cell props
-                      <td {...cell.getCellProps()}>
+                      <td
+                        {...cell.getCellProps()}
+                        className={`activities_table_data ${getHeaderSuffix(colIndex)}`}
+                      >
                         {
                           // Render the cell contents
                           cell.render("Cell")
