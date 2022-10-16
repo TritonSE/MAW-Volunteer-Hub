9e87b56ff0a4f33206853d46ebc45bf605ca3d29
James Zhang <jjz002@ucsd.edu>
2022-10-16 11:22:46 -0700

    Restore backend config.js

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 module.exports = {
   app: {
     env: process.env.NODE_ENV || "development",
-    port: process.env.PORT || "5001",
+    port: process.env.PORT || "5000",
+    frontend_url: process.env.FRONTEND_URL || "http://localhost:3000",
   },
   auth: {
     // register_secret: process.env.REGISTER_SECRET || "tritonse",
